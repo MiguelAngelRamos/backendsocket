83edf61863a83c8ad29dc71191df114c5feebb96
Miguel Ramos <return.infox@gmail.com>
2020-11-13 07:46:46 -0300

    Agregando cors por que la version 3 de socket io lo require

--- models/server.js
+++ models/server.js
@@ -5,7 +5,7 @@ const http = require('http');
 const socketio = require('socket.io');
 const path = require('path');
 const Sockets = require('./sockets');
-
+const cors = require('cors');
 class Server {
 
   constructor() {
@@ -24,6 +24,9 @@ class Server {
   middlewares() {
     // Desplegar directorio publico
     this.app.use(express.static(path.resolve(__dirname, '../public')));
+
+    // CORS
+    this.app.use(cors());
   }
 
   configurarSockets() {
